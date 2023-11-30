831b928b98dbe61c73e90fe1f824ae024a8cdf27
Furqan-sykss <takahirofurqan@gmail.com>
2023-11-30 23:51:35 +0700

    furqan2

--- lib/report/report1.dart
+++ lib/report/report1.dart
@@ -1,64 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class StaticBooks {
-  static const String cdn = "https://cdn.syncfusion.com/";
-  static const String path = "content/images/downloads/ebook/ebook-cover/";
-  static const List<String> covers = [
-    "visual-studio-for-mac-succinctly-v1.png",
-    "angular-testing-succinctly.png",
-    "azure-devops-succinctly.png",
-    "asp-net-core-3-1-succinctly.png",
-    "angulardart_succinctly.png"
-  ];
-}
-
-class Succinctly extends StatelessWidget {
-  List<Widget> createGrid() {
-    List<Widget> imgs = [];
-    Widget cImage;
-    for (int i = 0; i <= StaticBooks.covers.length - 1; i++) {
-      cImage = Container(
-          child: Image.network(
-              StaticBooks.cdn + StaticBooks.path + StaticBooks.covers[i]));
-      imgs.add(cImage);
-    }
-    return imgs;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final sizeX = MediaQuery.of(context).size.width;
-    final sizeY = MediaQuery.of(context).size.height;
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Succinctly Books'),
-        ),
-        body: Container(
-            width: sizeX,
-            height: sizeY,
-            child: GridView.count(
-              children: createGrid(),
-              padding: EdgeInsets.all(10),
-              crossAxisSpacing: 4.5,
-              mainAxisSpacing: 5.5,
-              crossAxisCount: 2,
-              scrollDirection: Axis.vertical,
-            )));
-  }
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Succinctly(),
-      theme: ThemeData(
-        primaryColor: Colors.indigo,
-        hintColor: Colors.amber,
-        brightness: Brightness.dark,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Halaman Report'),
+        ),
+        body: Center(
+          child: DataTable(
+            columns: [
+              DataColumn(label: Text('id_transaksi')),
+              DataColumn(label: Text('Nama')),
+              DataColumn(label: Text('No Pol')),
+              DataColumn(label: Text('Tanggal Penyewaan')),
+              DataColumn(label: Text('No Hp')),
+            ],
+            rows: [
+              DataRow(cells: [
+                DataCell(Text('01')),
+                DataCell(Text('Furqan')),
+                DataCell(Text('BL 2132 NY')),
+                DataCell(Text('18 Desember 2023')),
+                DataCell(Text('082238584400')),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('Alice')),
+                DataCell(Text('30')),
+                DataCell(Text('San Francisco')),
+                DataCell(Text('San Francisco')),
+                DataCell(Text('San Francisco')),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('Bob')),
+                DataCell(Text('28')),
+                DataCell(Text('Chicago')),
+                DataCell(Text('Chicago')),
+                DataCell(Text('Chicago')),
+              ]),
+            ],
+          ),
+        ),
       ),
     );
   }
