d664dd875cbb5f6bd8874949be67dd01575dea1b
ArgyaAziz <argyaagi98@gmail.com>
2023-12-04 11:30:21 +0700

    pengembalian

---
+++ lib/pengembalian/pengembalian1.dart
@@ -0,0 +1,13 @@
+void main() => runApp(MyApp());
+
+class StaticBooks {
+  static const String cdn = "https://cdn.syncfusion.com/";
+  static const String path = "content/images/downloads/ebook/ebook-cover/";
+  static const List<String> covers = [
+    "visual-studio-for-mac-succinctly-v1.png",
+    "angular-testing-succinctly.png",
+    "azure-devops-succinctly.png",
+    "asp-net-core-3-1-succinctly.png",
+    "angulardart_succinctly.png"
+  ];
+}
